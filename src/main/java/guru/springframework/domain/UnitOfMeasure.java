0b3b0daa01b119424ccb237ba89866da95d048f2
Kamil Gaborek <kamil.gaborek@motorolasolutions.com>
2020-04-07 20:12:03 +0200

    part1 implementation

---
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -0,0 +1,33 @@
+package guru.springframework.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+
+    private String uom;
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public String getUom() {
+        return uom;
+    }
+
+    public void setUom(String uom) {
+        this.uom = uom;
+    }
+}
